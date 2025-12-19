1fb8ab7e5ba96a38f4a48e85399c2d0cf8f25401
PoisonTree8 <poison8t@gmail.com>
2025-12-19 18:01:06 +0300

    Add basic routing and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx";
 
-const App = () => {
-
+export default function App() {
   return (
-    <h1>SHELF</h1>
+    <div style={{ padding: 20 }}>
+      <nav style={{ display: "flex", gap: 12, marginBottom: 16 }}>
+        <Link to="/">Home</Link>
+        <Link to="/books">Books</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+      </Routes>
+    </div>
   );
 }
-
-export default App
-
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Shelf</h1>
+      <p>Browse and buy digital books.</p>
+    </div>
+  );
+}
